5a8d83d1645c721bd5fef93dc718219c9f517e93
maria <106377401+mariabh7@users.noreply.github.com>
2023-07-13 19:47:01 +0100

    Add files via upload

---
+++ rating/rating.js
@@ -0,0 +1,24 @@
+const submit = document.getElementById("submit");
+const main1 = document.getElementById("main");
+const list = document.querySelectorAll(".number");
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener("click", (clicked) => {
+        list[i].style.border = "1px solid white";
+        list[i].style.backgroundColor = "white";
+        list[i].style.color = "hsl(25, 97%, 53%)";
+        submit.addEventListener("click", () => {
+            main1.innerHTML = `<div id="thank-you"><div class="th-img">
+  <img
+    src="file:///Users/mac/Downloads/interactive-rating-component-main/images/illustration-thank-you.svg"
+  />
+</div>
+<span id="rated"> You selected ${list[i].textContent} out of 5 </span>
+<h2>Thank you !</h2>
+<p class="rite">
+  We appreciate you taking the time to give a rating. If you ever need
+  more support . don't hesitate to get in touch!
+</p>
+</div>`
+        })
+    })
+}
